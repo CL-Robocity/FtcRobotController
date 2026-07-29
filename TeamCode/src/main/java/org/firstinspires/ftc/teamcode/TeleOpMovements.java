b6af6fe791d4284cae1673921a141f3cd0729379
PaoloneNuvolone <paolo.nuvola45@gmail.com>
2026-07-29 23:38:16 +0200

    messi sensori di distanza e CRservo
    se c'è qualcosa da cambiare con le nuove aggiunte, guardare le righe evidenziate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMovements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMovements.java
@@ -2,26 +2,39 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp (name = "New Arcade Drive - FGC",group = "TeleOp Competition")
 public class TeleOpMovements extends LinearOpMode {
+    private ElapsedTime timerServo = new ElapsedTime();
     private DcMotor leftMotor, rightMotor, upIntakeMotor, upIntakeSlowMotor;
     private DcMotorEx flywheel_left, flywheel_right;
-    private Servo servitore_right, servitore_left;
+    private Servo servitoreRight, servitoreLeft;
+    private TouchSensor touchSensorLeft, touchSensorRight;
+    private CRServo CRServoLeft, CRServoRight;
     private int currentPower = 0;
     private final int maxStep = 16;
-    private static final int TARGET_VELOCITY = 1600;
+    private static final int TARGET_VELOCITY = 2000;
     private static final double SERVO_CLOSE = 0;
-    private static final double SERVO_OPEN = 0.15;
+    private static final double SERVO_OPEN = 0.12;
     boolean flywheelActivate = false;
     boolean yStateBefore = false;
+    boolean servoIsMoving = false;
+    boolean barsOut = false;
+    boolean barsInside = true;
+    double crservoPower = 1.0;
 
     @Override
     public void runOpMode() throws InterruptedException{
+
+        /// INITIALIZING SENSORS
+        touchSensorLeft = hardwareMap.get(TouchSensor.class, "touch_sensor_left");
+        touchSensorRight = hardwareMap.get(TouchSensor.class, "touch_sensor_right");
 
         /// INITIALIZING MOTORS
         leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
@@ -29,8 +42,10 @@ public class TeleOpMovements extends LinearOpMode {
         upIntakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
         upIntakeSlowMotor = hardwareMap.get(DcMotor.class, "second_intake_motor");
 
-        servitore_right = hardwareMap.get(Servo.class, "servitore_1");
-        servitore_left = hardwareMap.get(Servo.class, "servitore_2");
+        servitoreRight = hardwareMap.get(Servo.class, "servitore_1");
+        servitoreLeft = hardwareMap.get(Servo.class, "servitore_2");
+        CRServoLeft = hardwareMap.get(CRServo.class, "CRServitore_left");
+        CRServoRight = hardwareMap.get(CRServo.class, "CRServitore_right");
 
         flywheel_right = hardwareMap.get(DcMotorEx.class, "flywheel_right");
         flywheel_left = hardwareMap.get(DcMotorEx.class, "flywheel_left");
@@ -44,7 +59,11 @@ public class TeleOpMovements extends LinearOpMode {
         flywheel_right.setDirection(DcMotorEx.Direction.FORWARD);
         flywheel_left.setDirection(DcMotorEx.Direction.FORWARD);
 
-        servitore_right.setDirection(Servo.Direction.REVERSE);
+        servitoreRight.setDirection(Servo.Direction.REVERSE);
+        servitoreLeft.setDirection(Servo.Direction.FORWARD);
+
+        CRServoRight.setDirection(CRServo.Direction.REVERSE);
+        CRServoLeft.setDirection(CRServo.Direction.FORWARD);
 
         /// RESETTING THE ENCODER
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -71,6 +90,7 @@ public class TeleOpMovements extends LinearOpMode {
         flywheel_right.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
         flywheel_left.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
 
+
         /// WHEN THE ROBOT IS READY, PRESS PLAY
         telemetry.addLine("[Initialized] Press Play to start");
         telemetry.update();
@@ -93,9 +113,10 @@ public class TeleOpMovements extends LinearOpMode {
             }
 
             // PRESS R2 FOR MAKING THE ROBOT GO SLOWER
+
             if (gamepad1.right_trigger_pressed){
-                leftPower*=0.6;
-                rightPower*=0.6;
+                leftPower*=0.8;
+                rightPower*=0.8;
             }
 
             // PRESS R2 FOR MAKING THE ROBOT GO SLOWER
@@ -107,7 +128,7 @@ public class TeleOpMovements extends LinearOpMode {
             leftMotor.setPower(leftPower);
             rightMotor.setPower(rightPower);
 
-            // INTAKE WITH R1 e R2
+            // INTAKE WITH R1 e OUTTAKE WITH R2
 
             if (gamepad1.right_bumper){
                 upIntakeMotor.setPower(-1);
@@ -138,17 +159,52 @@ public class TeleOpMovements extends LinearOpMode {
             // OPEN THE SERVOS AND MAKING THE INTAKE GO
 
             if (gamepad1.a){
-                servitore_right.setPosition(SERVO_OPEN);
-                servitore_left.setPosition(SERVO_OPEN);
+                servitoreRight.setPosition(SERVO_OPEN);
+                servitoreLeft.setPosition(SERVO_OPEN);
                 upIntakeMotor.setPower(-1);
                 upIntakeSlowMotor.setPower(-1);
             } else {
-                servitore_right.setPosition(SERVO_CLOSE);
-                servitore_left.setPosition(SERVO_CLOSE);
+                servitoreRight.setPosition(SERVO_CLOSE);
+                servitoreLeft.setPosition(SERVO_CLOSE);
                 if (!gamepad1.left_bumper && !gamepad1.right_bumper){
                     upIntakeMotor.setPower(0);
                     upIntakeSlowMotor.setPower(0);
                 }
+            }
+
+            // RETRACT THE EXTENDABLE BARS WITH D_PAD_UP AND D_PAD_DOWN
+
+            if (!servoIsMoving ){
+                if (gamepad1.dpad_up && !barsOut){
+                    crservoPower = -1.0;
+                    timerServo.reset();
+                    servoIsMoving = true;
+                } else if (gamepad1.dpad_down && !barsInside){
+                    crservoPower = 1.0;
+                    timerServo.reset();
+                    servoIsMoving = true;
+                }
+            }
+
+            if (servoIsMoving){
+                if(timerServo.seconds() <2.0){
+                    CRServoLeft.setPower(crservoPower);
+                    CRServoRight.setPower(crservoPower);
+                } else {
+                    CRServoRight.setPower(0);
+                    CRServoLeft.setPower(0);
+                    if (crservoPower < 0.0){
+                        barsOut = true;
+                        barsInside = false;
+                    } else if (crservoPower > 0.0){
+                        barsInside = true;
+                        barsOut = false;
+                    }
+                    servoIsMoving = false;
+                }
+            } else {
+                CRServoLeft.setPower(0);
+                CRServoRight.setPower(0);
             }
 
             // A SUMMARY FOR THE DRIVER
@@ -165,7 +221,6 @@ public class TeleOpMovements extends LinearOpMode {
         }
     }
 
-    // STA ROBA NON FUNZIONA, È DA GUARDARE
     public int update(int targetPower){
 
         int error = targetPower - currentPower;
